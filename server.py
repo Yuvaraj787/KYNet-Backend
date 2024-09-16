0b7208622d2fe37ef30af814e2325cc388f6015a
YUVARAJ787 <yuvarajv787@gmail.com>
2024-09-16 08:36:29 +0530

    file download

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 import csv
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/add_data', methods=['POST'])
 def add_data():
     # Get JSON data from request
-    data = eval(request.json)
+    data = request.json
 
     # Check if data is a list of lists
     if not isinstance(data, list) or not all(isinstance(row, list) for row in data):
@@ -19,5 +21,10 @@ def add_data():
 
     return jsonify({"message": "Data added successfully"}), 200
 
+@app.route('/data/network_data.csv', methods=['GET'])
+def send_data_file():
+    return send_file("./network_data.csv", as_attachment=True);
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
